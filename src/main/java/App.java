268792bcd55b8085a94e20eceef162e25ddcf6c3
Ankur Singh <68726977+ankur2802@users.noreply.github.com>
2026-06-21 14:27:41 +0530

    Update App.java

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -9,7 +9,7 @@ public class App {
 
 		System.out.println("cloned project from git, thi si first change");
 
-		System.out.println("cloned project from git, this is second change");
+		System.out.println("cloned project from git, this is second change, chnaging it again from git");
 
 	}
 
