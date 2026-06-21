74f8c5c8a2ad78b14c6ce69f9bbcd63a059565d5
ankur2802 <akrs.0028@gmail.com>
2026-06-21 14:00:02 +0530

    cloned from git and added on msg

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,8 @@ public class App {
 		System.out.println("Hello World - Fully Automated Pipeline!");
 		System.out.println("testing automation again");
 
+		System.out.println("cloned project from git, thi si first change");
+
 	}
 
 }
